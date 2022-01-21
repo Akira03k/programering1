effcec08035e0c76f12fb8e78a326b0361cc954e
MachineMade2003 <91880372+MachineMade2003@users.noreply.github.com>
2022-01-21 10:32:59 +0100

    Code written

--- hero-creator.py
+++ hero-creator.py
@@ -1,2 +1,26 @@
-heroes.insert("batman", "superman")
-print (heroes.insert)
+import random
+dc_first = ("Bat","Super", "Wonder", "The", "Green", "Aqua", "Cat")
+dc_last = ("man","woman","flash","lantern")
+marvel_first = ("Spider", "Iron", "The", "Black", "Thor", "Captain", "Doctor", "Ant")
+marvel_last = ("man", "hulk", "panther", "widow", "america", "strange")
+gen1 = random.sample(dc_first, 1)
+gen2 = random.sample(dc_last, 1)
+gen3 = random.sample(marvel_first, 1)
+gen4 = random.sample(marvel_last, 1)
+mega1 = gen1 + gen3
+mega2 = gen2 +gen4
+
+firstn = random.sample(mega1, 1)
+lastn = random.sample(mega2, 1)
+def listToString(s): 
+    
+    # initialize an empty string
+    str1 = "" 
+    
+    # traverse in the string  
+    for ele in s: 
+        str1 += ele  
+    
+    # return string  
+    return str1 
+print(listToString(firstn + lastn))
