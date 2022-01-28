fd453d2b9a0c9878efcf979209c16d86bee24f9e
RasmusHag <rhageryd@gmail.com>
2022-01-28 10:31:47 +0100

    Update hero-creator.py

--- hero-creator.py
+++ hero-creator.py
@@ -1,6 +1,6 @@
 import random
-dc_first = ("Bat","Super", "Wonder", "Green", "Aqua", "Cat")
-dc_last = ("man","woman","flash","lantern")
+dc_first = ("Bat","Super", "Wonder", "Green", "Aqua", "Cat", "Martian")
+dc_last = ("man","woman","flash","lantern", "manhunter")
 marvel_first = ("Spider", "Iron", "The", "Black", "Thor", "Captain", "Doctor", "Ant")
 marvel_last = ("man", "hulk", "panther", "widow", "america", "strange")
 gen1 = random.sample(dc_first, 1)
@@ -9,6 +9,8 @@ gen3 = random.sample(marvel_first, 1)
 gen4 = random.sample(marvel_last, 1)
 mega1 = gen1 + gen3
 mega2 = gen2 +gen4
+
+
 
 firstn = random.sample(mega1, 1)
 lastn = random.sample(mega2, 1)
